a94d2d6735a5edca8be5f4be32f9550e8e198654
Martin Zlámal <martin.zlamal@kiwi.com>
2017-04-18 19:04:03 +0200

    GraphQL: fix Booking test description

--- graphql/src/queries/__tests__/Booking.test.js
+++ graphql/src/queries/__tests__/Booking.test.js
@@ -4,13 +4,11 @@ import Booking from '../Booking';
 jest.mock('../../services/HttpRequest');
 
 describe('single booking query', () => {
-  it('should be Booking type', () => {
+  it('should be of Booking type', () => {
     expect(Booking.type.toString()).toBe('Booking');
   });
-});
 
-describe('arrival query', () => {
-  it('should return valid id field', async () => {
+  it('should return valid fields', async () => {
     const arrivalQuery = `{
       booking(id: 2707251) {
         arrival {
