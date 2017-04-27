d6a44c5f98e866caed660ea85ddfcc87f4c4b0ec
Martin Zlámal <mrtnzlml@gmail.com>
2017-04-27 16:29:30 +0200

    GraphQL: remove GraphQLIsoDateStub
    It's replaced by GeneralStub. Important note: this stub cannot be analyzed by Flow so it doesn't have @flow annotation.

--- graphql/config/stub/GraphQLIsoDateStub.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import {
-  GraphQLDate as date,
-  GraphQLDateTime as dateTime,
-} from 'graphql-iso-date';
-
-export const GraphQLDate = date;
-export const GraphQLDateTime = dateTime;
