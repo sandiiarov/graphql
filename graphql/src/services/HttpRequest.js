5c0c91b10fdff7ae4827fbfd6a39d4373bfbbbd0
Michal Sanger <michal.sanger@kiwi.com>
2017-05-03 12:08:26 +0200

    DRY in HttpRequest and tests fix

--- graphql/src/services/HttpRequest.js
+++ graphql/src/services/HttpRequest.js
@@ -4,19 +4,10 @@ import url from 'url';
 import fetch from 'node-fetch';
 import Logger from '../services/Logger';
 
-const request = async function request(
+export default function request(
   absoluteApiUrl: string,
   token: ?string,
 ): Promise<Object> {
-  if (process.env.NODE_ENV === 'test') {
-    throw new Error('HttpRequest should never be called in test environment.');
-  }
-  if (typeof absoluteApiUrl !== 'string') {
-    throw new Error(
-      `Relative API URL should be typeof 'string', '${typeof absoluteApiUrl}' given.`,
-    );
-  }
-
   const urlObject = url.parse(absoluteApiUrl, true);
   if (token !== null && token !== undefined) {
     if (urlObject.query === undefined) {
@@ -25,42 +16,49 @@ const request = async function request(
     urlObject.query.token = token;
   }
 
-  Logger.info(url.format(urlObject));
-  const response = await fetch(url.format(urlObject));
-
-  if (response.status !== 200) {
-    throw new Error(
-      `Proxied error ${response.status}: ${response.statusText} (${url.format(urlObject)})`,
-    );
-  }
-  return response.json();
-};
-
-export default request;
+  return fetchJson(url.format(urlObject));
+}
 
 export async function post(
   absoluteApiUrl: string,
   payload: Object,
   requestHeaders?: Object = {},
 ): Promise<Object> {
-  if (process.env.NODE_ENV === 'test') {
-    throw new Error('HttpRequest should never be called in test environment.');
-  }
-
   const body = JSON.stringify(payload);
   const headers = {
     'Content-Type': 'application/json',
     ...requestHeaders,
   };
-  const response = await fetch(absoluteApiUrl, {
-    body,
-    headers,
-    method: 'POST',
-  });
 
-  if (response.status !== 200) {
-    throw new Error(`${response.status}: ${response.statusText}`);
+  return fetchJson(absoluteApiUrl, 'POST', { body, headers });
+}
+
+async function fetchJson(
+  url: string,
+  method: string = 'GET',
+  options?: Object = {},
+): Promise<Object> {
+  if (process.env.NODE_ENV === 'test') {
+    throw new Error('HttpRequest should never be called in test environment.');
+  }
+  if (typeof url !== 'string') {
+    throw new Error(
+      `Relative API URL should be typeof 'string', '${typeof url}' given.`,
+    );
   }
 
+  const reqOptions = {
+    ...options,
+    method,
+  };
+
+  Logger.info(`${method} ${url}`);
+  const response = await fetch(url, reqOptions);
+
+  if (response.status !== 200) {
+    throw new Error(
+      `Proxied error ${response.status}: ${response.statusText} (${url})`,
+    );
+  }
   return response.json();
 }
