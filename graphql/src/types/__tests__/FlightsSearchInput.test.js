abedf04a4d878528243834c2a568c68a995a0045
Jakub Žaba <jakub.zaba@kiwi.com>
2017-06-05 17:35:16 +0200

    GraphQL: simplify type tests

--- graphql/src/types/__tests__/FlightsSearchInput.test.js
+++ graphql/src/types/__tests__/FlightsSearchInput.test.js
@@ -2,20 +2,6 @@
 
 import FlightsSearchInput from '../FlightsSearchInput';
 
-const fields = FlightsSearchInput.getFields();
-
-it('from field should be non-null String type', () => {
-  expect(fields.from.type.toString()).toBe('String!');
-});
-
-it('to field should be non-null String type', () => {
-  expect(fields.to.type.toString()).toBe('String!');
-});
-
-it('dateFrom field should be non-null Date type', () => {
-  expect(fields.dateFrom.type.toString()).toBe('Date!');
-});
-
-it('dateTo field should be non-null Date type', () => {
-  expect(fields.dateTo.type.toString()).toBe('Date!');
+it('FlightsSearchInput type should have valid fields', () => {
+  expect(FlightsSearchInput.getFields()).toMatchSnapshot();
 });
--- graphql/src/types/__tests__/Place.test.js
+++ graphql/src/types/__tests__/Place.test.js
@@ -2,24 +2,6 @@
 
 import Place from '../Place';
 
-const fields = Place.getFields();
-
-it('ID field should be non-null ID type', () => {
-  expect(fields.id.type.toString()).toBe('ID!');
-});
-
-it('Location field should be non-null Location type', () => {
-  expect(fields.location.type.toString()).toBe('Location!');
-});
-
-it('Name field should be non-null String type', () => {
-  expect(fields.name.type.toString()).toBe('String!');
-});
-
-it('NumberOfAirports field should be non-null Int type', () => {
-  expect(fields.numberOfAirports.type.toString()).toBe('Int!');
-});
-
-it('Population field should be nullable Int type', () => {
-  expect(fields.population.type.toString()).toBe('Int');
+it('Place type should have valid fields', () => {
+  expect(Place.getFields()).toMatchSnapshot();
 });
