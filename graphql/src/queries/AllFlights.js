4db545f914c20034c6da0953ca5597f966f451f1
Jakub Žaba <jakub.zaba@kiwi.com>
2017-06-08 16:35:26 +0200

    GraphQL: add fallback for AllFlights
    When AllFlights returns no value due to wrong from-to arguments (v3 allows to use codes ony) fetch locations with from-to values and use first location in new AllFlights request.

--- graphql/src/queries/AllFlights.js
+++ graphql/src/queries/AllFlights.js
@@ -27,7 +27,7 @@ export default {
   ): Promise<Array<FlightType>> => {
     validateArgs(args);
 
-    const allFlights = await request(
+    let allFlights = await request(
       config.restApiEndpoint.allFlights({
         flyFrom: args.search.from,
         to: args.search.to,
@@ -37,6 +37,10 @@ export default {
       }),
     );
 
+    if (allFlights._results === 0) {
+      allFlights = await fallBack(args);
+    }
+
     const flightsMetadata = {
       currency: allFlights.currency,
     };
@@ -45,6 +49,47 @@ export default {
     );
   },
 };
+
+async function fallBack(args: Object) {
+  const { from, to } = await fetchLocationIds(args.search.from, args.search.to);
+
+  return request(
+    config.restApiEndpoint.allFlights({
+      flyFrom: from,
+      to: to,
+      dateFrom: dateFns.format(new Date(args.search.dateFrom), 'DD/MM/YYYY'),
+      dateTo: dateFns.format(new Date(args.search.dateTo), 'DD/MM/YYYY'),
+    }),
+  );
+}
+
+async function fetchLocationIds(from: string, to: string) {
+  const [locationsFrom, locationsTo] = await Promise.all([
+    // Location from
+    request(
+      config.restApiEndpoint.allLocations({
+        term: from,
+      }),
+    ),
+    // Location to
+    request(
+      config.restApiEndpoint.allLocations({
+        term: to,
+      }),
+    ),
+  ]);
+
+  if (locationsFrom.locations.length === 0) {
+    throw new Error(`Origin '${from}' has not been found.`);
+  } else if (locationsTo.locations.length === 0) {
+    throw new Error(`Destination '${to}' has not been found.`);
+  }
+
+  return {
+    from: locationsFrom.locations[0].id,
+    to: locationsTo.locations[0].id,
+  };
+}
 
 type FlightsMetadataType = {
   currency: string,
---
+++ graphql/src/queries/__tests__/AllFlights.fallback.test.js
@@ -0,0 +1,30 @@
+// @flow
+
+import { executeQuery } from '../../services/TestingTools';
+
+describe('all flights location fallback', () => {
+  it('should return legs data by using locations fallback', async () => {
+    const allFlightsSearchQuery = `
+    query ($input: FlightsSearchInput!) {
+      allFlights(search: $input) {
+        legs {
+          id
+          departure { airport { city { name } } }
+          arrival { airport { city { name } } }
+        }
+      }
+    }`;
+    const variables = {
+      input: {
+        // From and To defined as a name, not code or id
+        from: 'Prague',
+        to: 'Mexico',
+        dateFrom: '2017-08-08',
+        dateTo: '2017-09-08',
+      },
+    };
+    expect(
+      await executeQuery(allFlightsSearchQuery, variables),
+    ).toMatchSnapshot();
+  });
+});
