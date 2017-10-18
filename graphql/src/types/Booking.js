9803715f38400084eafd5e5773bebac2adbf6926
Martin Zlámal <mrtnzlml@gmail.com>
2017-10-18 15:50:56 -0500

    GraphQL: expose booking assets

--- graphql/src/types/Booking.js
+++ graphql/src/types/Booking.js
@@ -4,6 +4,9 @@ import type { DepartureArrival, Leg } from './Flight';
 import type { Price } from './Price';
 import type { AllowedBaggage } from './Baggage';
 
+/**
+ * This is fetched from 'bookings' endpoint.
+ */
 export type BookingsItem = {
   id: number,
   arrival: DepartureArrival,
@@ -13,6 +16,15 @@ export type BookingsItem = {
   authToken: string,
 };
 
+/**
+ * This is additionally fetched from 'booking/12..' endpoint if needed.
+ */
 export type Booking = BookingsItem & {
   allowedBaggage: AllowedBaggage,
+  assets: BookingAssets,
+};
+
+export type BookingAssets = {
+  ticketUrl: string,
+  invoiceUrl: string,
 };
