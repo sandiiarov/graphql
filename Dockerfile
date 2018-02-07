69b40c374a4e0e7087e45c70bfcd90303fed0a01
Martin Zlámal <mrtnzlml@gmail.com>
2018-02-07 13:22:29 +0200

    Add basic Docker image and Babel build script

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:9.5.0
+
+WORKDIR /graphql
+COPY .build/ .build/
+COPY node_modules/ node_modules/
+
+EXPOSE 3000
+CMD ["node", "/graphql/.build/src/index.js"]
---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# see: https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -e # exit immediately if a pipeline returns a non-zero status
+
+yarn run babel config --out-dir .build/config --ignore __tests__,__mocks__
+yarn run babel src --out-dir .build/src --ignore __tests__,__mocks__
+# TODO: install only production dependencies (Docker)
