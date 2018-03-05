6fdada17cdb1d3bdd9054bd8e02b1daa999c6f71
Michal Sanger <michal.sanger@gmail.com>
2018-03-05 19:58:17 +0100

    Locations tests refactored

--- src/location/dataloaders/__tests__/Location.test.js
+++ src/location/dataloaders/__tests__/Location.test.js
@@ -27,6 +27,13 @@ beforeEach(() => {
       term: 'this location does not exist',
     }),
   ).replyWithData(locationsJsonUnknown);
+
+  RestApiMock.onGet(
+    options.restApiEndpoint.allLocations({
+      type: 'id',
+      id: 'unknown',
+    }),
+  ).replyWithData(locationsJsonUnknown);
 });
 
 it('returns single PRG result', async () => {
@@ -42,8 +49,17 @@ it('returns single PRG and BRQ result', async () => {
 });
 
 it('throws error when accessing unknown location', async () => {
+  expect.assertions(1);
   const dataloader = new Location(new LocationSuggestions());
   await expect(
     dataloader.loadMany(['this location does not exist']),
   ).rejects.toEqual(new Error('Location has not been found.'));
+});
+
+it('throws error when loading unknown location by ID', async () => {
+  expect.assertions(1);
+  const dataloader = new Location(new LocationSuggestions());
+  await expect(dataloader.loadById('unknown')).rejects.toEqual(
+    new Error('Location has not been found.'),
+  );
 });
