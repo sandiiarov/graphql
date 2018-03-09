2935ac258a32807f6dd3878cc48b9a4dc212dccd
Michal Sanger <michal.sanger@gmail.com>
2018-03-09 18:13:28 +0100

    Simple server when missing Engine key

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 // @flow
 
+import http from 'http';
 import { ApolloEngine } from 'apollo-engine';
 import app from './graphqlServer';
 
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-const engine = new ApolloEngine({
-  apiKey: String(process.env.ENGINE_KEY),
-});
 
-engine.listen({
-  port,
-  expressApp: app,
-  graphqlPaths: ['/'],
-  frontend: {
-    extensions: {
-      strip: ['tracing'],
+if (process.env.ENGINE_KEY) {
+  const engine = new ApolloEngine({
+    apiKey: String(process.env.ENGINE_KEY),
+  });
+
+  engine.listen({
+    port,
+    expressApp: app,
+    graphqlPaths: ['/'],
+    frontend: {
+      extensions: {
+        strip: ['tracing'],
+      },
     },
-  },
-});
+  });
+} else {
+  // no ENGINE_KEY, start simple server
+  http.createServer(app).listen(
+    port.toString(),
+    () => console.log(`Server is running on port ${port}`), // eslint-disable-line no-console
+  );
+}
