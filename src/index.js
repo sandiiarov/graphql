cdc5a941b0f7c24c9d1568b5eadc14a07df0797c
Simon Prochazka <simon@fallion.net>
2018-03-09 13:06:36 +0100

    add Apollo Engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 // @flow
 
-import http from 'http';
+import { ApolloEngine } from 'apollo-engine';
 import app from './graphqlServer';
 
-const PORT = '3000';
-http
-  .createServer(app)
-  .listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // eslint-disable-line no-console
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const engine = new ApolloEngine({
+  apiKey: String(process.env.ENGINE_KEY),
+});
+
+engine.listen({
+  port,
+  expressApp: app,
+  graphqlPaths: ['/'],
+  frontend: {
+    extensions: {
+      strip: ['tracing'],
+    },
+  },
+});
