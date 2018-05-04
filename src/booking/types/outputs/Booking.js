5f28422580b8c5e652ce7ab27ec26bb3a0cb0f83
Martin Zlámal <mrtnzlml@gmail.com>
2018-05-04 12:51:45 +0200

    Booking: introduce new 'destinationImageUrl' field

--- src/booking/types/outputs/Booking.js
+++ src/booking/types/outputs/Booking.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLEnumType, GraphQLObjectType, GraphQLString } from 'graphql';
 
 import type { Booking } from '../../Booking';
 import BookingOneWay, { type BookingOneWayData } from './BookingOneWay';
@@ -13,6 +13,7 @@ import BookingMulticity, {
   type BookingMulticityData,
   createTrips,
 } from './BookingMulticity';
+import BookingDestinationImageURL from '../../resolvers/BookingDestinationImageURL';
 import DeprecatedField from './Booking.deprecated';
 
 export default new GraphQLObjectType({
@@ -66,6 +67,30 @@ export default new GraphQLObjectType({
 
         return { ...booking, trips };
       },
+    },
+
+    destinationImageUrl: {
+      type: GraphQLString,
+      args: {
+        dimensions: {
+          type: new GraphQLEnumType({
+            name: 'BookingDestinationImageDimensions',
+            values: {
+              _1200x628: { value: '1200x628' },
+              _1280x720: { value: '1280x720' },
+              _220x165: { value: '220x165' },
+              _275x250: { value: '275x250' },
+              _300x165: { value: '300x165' },
+              _375x165: { value: '375x165' },
+              _600x330: { value: '600x330' },
+              _600x600: { value: '600x600' },
+              _610x251: { value: '610x251' },
+            },
+            defaultValue: '600x600',
+          }),
+        },
+      },
+      resolve: BookingDestinationImageURL,
     },
   },
 });
